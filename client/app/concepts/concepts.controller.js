daf1f9fce4d8eefe0c095551f10900c593c5889e
Ricardo Ibarra <ribarra@tekmexico.com>
2015-07-17 17:18:22 -0700

    Added validation messages

--- client/app/concepts/concepts.controller.js
+++ client/app/concepts/concepts.controller.js
@@ -1,9 +1,31 @@
 'use strict';
 
 angular.module('newappApp')
-  .controller('ConceptsCtrl', function ($scope) {
+  .controller('ConceptsCtrl', function ($scope, $http) {
     $scope.save = function(form) {
       $scope.submitted = true;
-      console.log(form);
+      $scope.success = false;
+      $scope.error = false;
+      if(form.$valid) {
+        $http.post('api/concepts',
+          {
+            name:$scope.concept.title,
+            info:$scope.concept.description,
+            active:$scope.concept.status
+          }).then(function() {
+              $scope.last_saved = $scope.concept.title;
+              $scope.success = true;
+              //$scope.concept.title = "";
+            /*console.log(form);
+            form.$setUntouched();*/
+              $scope.form.$setPristine();
+
+
+          }).catch(function() {
+              $scope.error = true;
+          });
+
+
+      }
     }
   });
