81aa77a19f2a8dfbbed514246ace7641989c6628
Ricardo Ibarra <coder10@gmail.com>
2015-07-20 19:40:52 -0700

    added progress bar

--- client/app/concepts/edit/concepts/edit.controller.js
+++ client/app/concepts/edit/concepts/edit.controller.js
@@ -7,16 +7,19 @@ angular.module('newappApp')
     $scope._id = undefined;
     $scope.concept = {};
     $scope.concept.status = true;
+    $scope.bgwork = false;
 
     if($routeParams.id) {
       $scope._id = $routeParams.id;
       //must show a loading message
+      $scope.bgwork = true;
       $http.get('api/concepts/' + $scope._id).success(function(data) {
           $scope.concept.title = data.name;
           $scope.concept.description = data.info;
           $scope.concept.status = data.active||false;
       }).then(function() {
         //must hide loading
+        $scope.bgwork = false;
       });
     }
 
@@ -26,6 +29,7 @@ angular.module('newappApp')
       $scope.submitError = false;
 
       if(form.$valid) {
+        $scope.bgwork = true;
         $http.put('api/concepts/' + $scope._id, {
           name:$scope.concept.title,
           info:$scope.concept.description,
@@ -43,6 +47,8 @@ angular.module('newappApp')
           }, 3000);
         }).catch(function() {
           $scope.submitError = true;
+        }).then(function() {
+          $scope.bgwork = false;
         });
       }
     };
