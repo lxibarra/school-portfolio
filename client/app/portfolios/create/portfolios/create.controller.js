0a8ca41ed7b660e2a0f35299ed2c0f41bc6cf2c3
Ricardo Ibarra <coder10@gmail.com>
2015-07-23 23:13:24 -0700

    working on portfolio creation concept selection

--- client/app/portfolios/create/portfolios/create.controller.js
+++ client/app/portfolios/create/portfolios/create.controller.js
@@ -1,6 +1,14 @@
 'use strict';
 
 angular.module('newappApp')
-  .controller('PortfoliosCreateCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('PortfoliosCreateCtrl', function ($scope, $http) {
+    $http.get('api/concepts').then(function(data) {
+      $scope.concepts = data.data;  
+    }, function() {
+      console.log('Error');
+    });
+    
+    $scope.save = function() {
+      console.log($scope);
+    }
   });
