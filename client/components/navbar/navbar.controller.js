5c8770c62b59155599e291fbc9f243e81e578965
Ricardo Ibarra <coder10@gmail.com>
2015-09-01 21:26:50 -0700

    Added nav option highlight

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -2,22 +2,24 @@
 
 angular.module('newappApp')
   .controller('NavbarCtrl', function ($scope, $location, Auth) {
-    $scope.menu = [{
-      'title': 'Home',
-      'link': '/'
-    }];
+
 
     $scope.isCollapsed = true;
     $scope.isLoggedIn = Auth.isLoggedIn;
     $scope.isAdmin = Auth.isAdmin;
     $scope.getCurrentUser = Auth.getCurrentUser;
 
-    $scope.logout = function() {
+    $scope.logout = function () {
       Auth.logout();
       $location.path('/login');
     };
 
-    $scope.isActive = function(route) {
-      return route === $location.path();
+    $scope.isActive = function (route, reg) {
+      if (!reg) {
+        return route === $location.path();
+      } else {
+        var routes = RegExp(route, 'i');
+        return routes.test($location.path());
+      }
     };
   });
