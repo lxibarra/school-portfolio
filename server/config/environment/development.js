c968b99c33d01fa69af633c0c9b4ff516c7bc815
Ricardo Ibarra <coder10@gmail.com>
2015-09-12 15:35:53 -0700

    Changed production settings

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -8,7 +8,7 @@ module.exports = {
     uri: 'mongodb://localhost/newapp-dev'
   },
 
-  seedDB: true,
+  seedDB: false,
 
   
 };
--- server/config/local.env.sample.js
+++ server/config/local.env.sample.js
@@ -6,19 +6,23 @@
 // You will need to set these on the server you deploy to.
 
 module.exports = {
-  AWS:'Amazon',
-  DOMAIN:           'http://localhost:9000',
-  SESSION_SECRET:   'newapp-secret',
+  AWS_S3_KEY: 'your-aws-key',
+  AWS_S3_SECRET: 'your-aws-secret',
+  AWS_S3_BUCKET: 'school-portfolio',
+  reCaptchaSecret:'googles-reCaptcha-secret',
+  DOMAIN: 'app-domain-name',
+  SESSION_SECRET: "your-app-session-secret",
+  MYVAR:"MyAppName",
 
-  FACEBOOK_ID:      'app-id',
-  FACEBOOK_SECRET:  'secret',
+  FACEBOOK_ID: 'app-id',
+  FACEBOOK_SECRET: 'secret',
 
-  TWITTER_ID:       'app-id',
-  TWITTER_SECRET:   'secret',
+  TWITTER_ID: 'app-id',
+  TWITTER_SECRET: 'secret',
 
-  GOOGLE_ID:        'app-id',
-  GOOGLE_SECRET:    'secret',
+  GOOGLE_ID: 'app-id',
+  GOOGLE_SECRET: 'secret',
 
   // Control debug level for modules using visionmedia/debug
-  DEBUG: ''
+  DEBUG: '',
 };
