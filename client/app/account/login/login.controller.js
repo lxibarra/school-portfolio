12dccdf80b281e9419b5ee0ff61c6ef4181b8a32
Ricardo Ibarra <coder10@gmail.com>
2015-09-19 23:07:59 -0700

    Added a few css fixes

--- client/app/account/login/login.controller.js
+++ client/app/account/login/login.controller.js
@@ -4,16 +4,19 @@ angular.module('newappApp')
   .controller('LoginCtrl', function ($scope, Auth, $location, $window) {
     $scope.user = {};
     $scope.errors = {};
+    $scope.bgwork = false;
 
     $scope.login = function(form) {
       $scope.submitted = true;
 
       if(form.$valid) {
+        $scope.bgwork = true;
         Auth.login({
           email: $scope.user.email,
           password: $scope.user.password
         })
         .then( function() {
+            $scope.bgwork =false;
           // Logged in, redirect to home
           $location.path('/');
         })
@@ -22,6 +25,7 @@ angular.module('newappApp')
             $location.path('/inactiveuser');
           }
           $scope.errors.other = err.message;
+            $scope.bgwork = false;
         });
       }
     };
